bbbfa255138892109de4814c4ee4ab12dc294556
naweasleey <nailyna1212@gmail.com>
2022-12-26 21:57:45 +0700

    add model static

---
+++ IntegerWebApplication/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace IntegerWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; } 
+        public string PhoneNumber { get; set; }
+
+    }
+}
