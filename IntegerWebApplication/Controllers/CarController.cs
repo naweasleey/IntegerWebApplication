5a1d73428fff03a78e9e798ec2d6ae392eba906d
naweasleey <nailyna1212@gmail.com>
2023-01-02 09:07:51 +0700

    Pertemuan 3

---
+++ IntegerWebApplication/Controllers/CarController.cs
@@ -0,0 +1,68 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            //NAMBAH DATA TANPA ARRAY
+            var cars = new List<Car>(); //deklarasi 
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 1,
+                Type = "Sedan",
+                Brand = "Toyota",
+                Varian = "Black"
+            });
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 2,
+                Type = "Sedan",
+                Brand = "BMW",
+                Varian = "Soft Purple"
+            });
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 3,
+                Type = "SUV",
+                Brand = "Hyundai",
+                Varian = "Black Doff"
+            });
+
+
+            //NAMBAH DATA BANYAK
+            //var cars = new Car[] //array
+            //{
+            //    new Car{IDRegistration = 1, Type="Sedan", Brand="Toyota", Varian="Black"},
+            //    new Car{IDRegistration = 2, Type="Sedan", Brand="BMW", Varian="Soft Purple"},
+            //    new Car{IDRegistration = 3, Type="SUV", Brand="Hyundai", Varian="Black Doff"},
+            //    new Car{IDRegistration = 4, Type="Honda", Brand="CRV", Varian="Maroon"}
+
+
+            //};
+            //ViewBag.Cars = cars; //untuk data yang lebih dari satu
+
+            //MENGHAPUS DATA
+            //var car1 = cars.Single(x => x.IDRegistration == 2);
+            //cars.Remove(car1);
+
+            //CARI
+            //var car = cars.Where(x => x.Type == "Sedan");
+            //ViewBag.Cars = car;
+
+            var car = cars.Where(x => x.Type == "Sedan").LastOrDefault(); //FirstOrDefault
+            ViewBag.Cars = new List<Car>() { car};
+
+           
+
+
+
+            return View();
+        }
+    }
+}
---
+++ IntegerWebApplication/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntegerWebApplication.Models
+{
+    public class Car
+    {
+        //[Key] //untuk menandakan kalo itu primary key
+        //biasanya variabel yang ada 'id' nya otomatis jadi primary key
+        public int IDRegistration { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Varian { get; set; }
+
+    }
+}
+
+// cara membuat model otomatis dengan view dengan yang ada mvcnya
+//nama controller harus sama dengan folder di view
